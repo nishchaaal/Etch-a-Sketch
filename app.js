44fdba0d4f99f0bbdc5a90f6fbe0c04361e9b48f
nishchaaal <nishchalpunn@gmail.com>
2021-11-20 20:31:52 +0545

    updates

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ let noOfCubes = 16;
 let roomWidth = main.offsetWidth / noOfCubes ;
 
 // Functions
+
+const randomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 const gridMaker = (numb) => {
     for (let i = 0; i < numb*numb; i++) {
         const items = document.createElement('div');
@@ -14,21 +24,30 @@ const gridMaker = (numb) => {
     };
     const box = document.querySelectorAll('.cube');
     box.forEach((room) => {
+        room.style.backgroundColor = 'rgb(255, 255, 255)';
+
         room.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
+                let generatedColor = randomColor();
+                e.target.style.backgroundColor = generatedColor;
         })
     })
     main.style.gridTemplateColumns = `repeat(${numb}, 1fr)`
 }
 
+const enterNewSize = () => {
+    let newValue = prompt('Enter the new size(1-99)');
+    if(0 < newValue && newValue < 100) {
+        const box = document.querySelectorAll('.cube');
+        box.forEach((room) => {
+            main.removeChild(room);
+        })
+        gridMaker(newValue);
+    } else {
+        alert('Numbers only from 1 to 99');
+        return enterNewSize();
+    }
+}
+
+btn.addEventListener('click', enterNewSize);
+
 gridMaker(noOfCubes);
-
-btn.addEventListener('click', () => {
-    let newValue = prompt('Enter the new size');
-    const box = document.querySelectorAll('.cube');
-    box.forEach((room) => {
-        main.removeChild(room);
-    })
-    gridMaker(newValue);
-});
-
